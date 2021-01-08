bc5b93e91743047bc6160d04c4283bc057d67aaf
kmchord9 <kmchord9@KM-S04.local>
2021-01-08 16:10:11 +0900

    モデル定義を時刻と値に変更

--- src/project/test.js
+++ src/project/test.js
@@ -10,10 +10,14 @@ var uri = 'mongodb://docker_mongo_mongo_1:27017/temp';
 
 mongoose.connect(uri, options);
 
-const yourSchema = new mongoose.Schema({ name: 'String', size: 'String' });
-const Tank = mongoose.model('Tank', yourSchema); 
 
-const small = new Tank({name: 'Yasu2', size: 'large2'});
+const TempDataSchema = new mongoose.Schema({
+  addtime: Date,
+  tempdata: Number
+});
+const SomeModel = mongoose.model('SomeModel', TempDataSchema );
+
+const small = new SomeModel({addtime: new Date(), tempdata: Math.random()});
 
 small.save(err => {
   if (err) console.error(err)
