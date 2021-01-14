31c8184bed927fa536d7ed4de00d5892deb2a87b
kmchord9 <kmchord9@gmail.com>
2021-01-14 16:04:52 +0900

    非同期の繰り返しによるデータ保存を追加

--- src/project/test.js
+++ src/project/test.js
@@ -15,25 +15,37 @@ const TempDataSchema = new mongoose.Schema({
   addtime: Date,
   tempdata: Number
 });
-const SomeModel = mongoose.model('SomeModel', TempDataSchema );
 
-//var id = setInterval(function () {
-	
-	const small = new SomeModel({addtime: new Date(), tempdata: Math.random()});
-/*	
-	small.save(err => {
-	if (err) console.error(err)
-	console.log('saved')
-	});
-	
-	console.log('inloop');
-},8000);
-*/
+function sleep(second) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve()
+        }, second * 1000)
+    })
+}
 
 
-small.save(err => {
-  if (err) console.error(err)
-  console.log('saved')
+function modelSave() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+		  const SomeModel = mongoose.model('SomeModel', TempDataSchema );
+		  const small = new SomeModel({addtime: new Date(), tempdata: Math.random()});
+			small.save().then(err => {
+				if(err) console.error(err);
+				resolve();
+    },2000);
+    } )
+  })
+};
+
+(async () => {
+	for (let i=0; i<5; i++) {
+		await modelSave().then( resolve =>{ console.log("finish")	});
+		await sleep(1);
+		console.log("done")
+  }
+  
   mongoose.disconnect();
-});
+  console.log("disconnected");
+})();
 
