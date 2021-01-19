1e06d328338c18aab6b266bb51d0ef4d71a68fa1
kmchord9 <kmchord9@gmail.com>
2021-01-19 13:08:38 +0900

    apiserver追加

---
+++ src/api/server.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var	app = express();
+var	port = process.envPORT || 3000;
+var mongoose = require("mongoose");
+var	bodyParser = require("body-parser");
+
+mongoose.Promise = global.Promise;
+
+var options = {
+	user: 'usertemp',
+	pass: 'usertemp',
+	useUnifiedTopology: true
+
+};
+
+var uri = 'mongodb://dockermongo_mongo_1:27017/temp'
+
+mongoose.connect(uri, options);
+
+var	Task = require("./api/models/taskModel");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require("./api/routes/taskRoutes");
+routes(app);
+
+app.listen(port);
+
+console.log("todo list RESTfull API server started on: "+ port);
