de2c88d810b548c193baa03be23d21de731be093
kmchord9 <kmchord9@gmial.com>
2021-01-27 18:24:52 +0900

    realtimeの更新グラフサンプル作成

--- src/chart-js/src/App.js
+++ src/chart-js/src/App.js
@@ -1,36 +1,46 @@
 import React from 'react';
-import { Bar } from 'react-chartjs-2';
+import { Line } from 'react-chartjs-2';
+import 'chartjs-plugin-streaming';
 //import logo from './logo.svg';
 import './App.css';
 
 function App() {
-	const graphData = {
-		labels: [
-      ['2019', '1月'],
-      ['2019', '2月'],
-      ['2019', '3月'],
-      ['2019', '4月'],
-      ['2019', '5月'],
-      ['2019', '6月'],
-      ['2019', '7月'],
-      ['2019', '8月'],
-      ['2019', '9月'],
-      ['2019', '10月'],
-      ['2019', '11月'],
-      ['2019', '12月'],
-		],
-    datasets: [
-      {
-        data: [16,42,117,90,120,225,235,112,116,457,43,23],
-        label: '月別降水量(mm)',
-      },
-    ]
-  };
-  return (
-    <div className="App">
-      <Bar data={graphData} />
-    </div>
-  );
-}
+    function onRefresh(chart) {
+      chart.config.data.datasets.forEach(function(dataset) {
+       dataset.data.push({
+        x: Date.now(),
+        y: Math.random()
+       });
+      });
+    };
+    
+    const graphData = {
+      datasets: [
+        {
+          data: [],
+        },
+        {
+          data: [],
+        }
+      ]
+    };
 
+    const optionData = {
+      scales: {
+       xAxes: [{
+        type: 'realtime',
+        realtime: {
+          onRefresh: onRefresh,
+          delay: 2000
+        },
+       }]
+      }
+    };
+
+    return (
+      <div className="App">
+        <Line data={graphData} options={optionData}/>
+      </div>
+    );
+}
 export default App;
